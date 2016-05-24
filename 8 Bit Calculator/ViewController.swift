a31ae593e947127766a93ae4883a701c29ec71a4
Vikash Loomba <loomba.vikash@gmail.com>
2016-05-24 16:00:55 -0700

    UI, button functions

--- 8 Bit Calculator/ViewController.swift
+++ 8 Bit Calculator/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    enum Operation: String {
+        case Multiply = "*"
+        case Divide = "/"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var buttonSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftNumber = ""
+    var rightNumber = ""
+    var currentOperation: Operation = Operation.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Grabbing audio file for button clicks
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        //Error handling if AVFoundation cant load URL
+        do {
+            try buttonSound = AVAudioPlayer(contentsOfURL: soundURL)
+            buttonSound.prepareToPlay()
+        } catch let error as NSError{
+            //print the error for debugging
+            print(error.debugDescription)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func numberPressed(btn: UIButton!){
+        buttonSound.play()
     }
-
+    
+    @IBAction func onDividePressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        
+    }
+    
 
 }
 
